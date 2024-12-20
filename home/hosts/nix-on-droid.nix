c10c69b248b0f4b59900b55ca79066174e8d08bd
esi <git@esibun.net>
2024-12-20 03:11:59 -0500

    android: use FiraCode NF (hack to copy non-symlink)

--- home/hosts/nix-on-droid.nix
+++ home/hosts/nix-on-droid.nix
@@ -1,4 +1,4 @@
-{pkgs, ...}:
+{pkgs, lib, ...}:
 
 {
   imports = [
@@ -9,5 +9,15 @@
     homeDirectory = "/data/data/com.termux.nix/files/home";
     stateVersion = "24.05";
     username = "nix-on-droid";
+
+    # taken from nix-on-droid#120
+    activation = {
+      copyFont = let
+          font_src = "${pkgs.nerdfonts.override { fonts = [ "FiraCode" ]; }}/share/fonts/truetype/NerdFonts/Fira Code Regular Nerd Font Complete Mono.ttf";
+          font_dst = "${config.home.homeDirectory}/.termux/font.ttf";
+        in lib.hm.dag.entryAfter ["writeBoundary"] ''
+          ( test ! -e "${font_dst}" || test $(sha1sum "${font_src}"|cut -d' ' -f1 ) != $(sha1sum "${font_dst}" |cut -d' ' -f1)) && $DRY_RUN_CMD install $VERBOSE_ARG -D "${font_src}" "${font_dst}"
+      '';
+    };
   };
 }
