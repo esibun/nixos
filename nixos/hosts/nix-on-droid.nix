bfe96b8af2f5f24983442fee6cbd69cf4cff9d7e
esi <git@esibun.net>
2024-12-20 16:11:22 -0500

    android: add some missing base utilities

--- nixos/hosts/nix-on-droid.nix
+++ nixos/hosts/nix-on-droid.nix
@@ -1,11 +1,21 @@
 {pkgs, ...}:
 
 {
-  # TODO: import requiredPackages here from base nix
+  # TODO: figure out all base config that's missing on nix-on-droid
+  environment.systemPackages = with pkgs; [
+    findutils
+    gnugrep
+    openssh
+    which
+  ];
 
   home-manager = {
     config = ../../home/hosts/nix-on-droid.nix;
     useGlobalPkgs = true;
+  };
+
+  programs = {
+    command-not-found.enable = true;
   };
 
   system.stateVersion = "24.05";
