9c083b4947467977717c1679f6d238e0a044264f
esi <git@esibun.net>
2024-12-22 05:55:36 -0500

    apparently zen doesnt actually support SCHED_ISO, gamemode seems to be enough to solve stuttering

--- nixos/modules/gaming.nix
+++ nixos/modules/gaming.nix
@@ -1,9 +1,6 @@
 {pkgs, lib, ...}:
 
 {
-  # use kernel that supports SCHED_ISO for gamemode
-  boot.kernelPackages = lib.mkForce pkgs.linuxPackages_zen;
-
   programs.gamemode = {
     enable = true;
     settings = {
