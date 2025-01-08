883b3c06c5012973bc2c2ec72b65e49649bfd8d1
esi <git@esibun.net>
2025-01-07 20:11:59 -0500

    vesktop: this patch probably should be documented

--- home/profiles/desktop.nix
+++ home/profiles/desktop.nix
@@ -84,6 +84,8 @@ in
         pnpmDeps = unstable.vesktop.pnpmDeps;
         patches = unstable.vesktop.patches ++
           [(pkgs.fetchpatch {
+            # Pull PR: Allow streaming from camera devices
+            #  There's some interest in upstreaming but the PR itself is a year old.
             url = "https://patch-diff.githubusercontent.com/raw/Vencord/Vesktop/pull/195.patch";
             hash = "sha256-ArBtQGJu5pDFPNd+tGhsI62PJBaE40zEBwm2ynL5vDQ=";
           })];
