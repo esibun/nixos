6a2209a5a350908dc19c23df7d9c1474eafad3c4
esi <git@esibun.net>
2025-04-14 20:21:45 -0400

    add beatoraja to native game list since this was apparently still going through lutris

--- home/profiles/gaming-beatoraja.nix
+++ home/profiles/gaming-beatoraja.nix
@@ -1,7 +1,22 @@
-{pkgs, ...}:
+{pkgs, config, ...}:
 
 {
   home.packages = with pkgs; [
     steam-run
+
+    (callPackage ../pkgs/native-game.nix {
+      title = "Beatoraja";
+      baseDir = "/mnt/nvme/bms/LR2oraja";
+      shortname = "beatoraja";
+      mainBinary = "beatoraja.sh";
+      # TODO: icon?
+      gamescopeFlags = config.gamescopeFlags;
+      extraBin = [
+        xorg.xrandr
+      ];
+      commandPrefix = "env SHUT_UP_TACHI=yes";
+      gamePrefix = "${pkgs.mangohud}/bin/mangohud ${pkgs.obs-studio-plugins.obs-vkcapture}/bin/obs-gamecapture";
+      commandPostfix = "-s";
+    })
   ];
 }
