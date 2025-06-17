e1ad2b9e19c5e80ee7d6809ec2a8f6070f3054f4
esi <git@esibun.net>
2025-06-17 18:07:31 -0400

    comment out udev rules temporarily to fix build

--- nixos/hosts/esi-laptop.nix
+++ nixos/hosts/esi-laptop.nix
@@ -7,13 +7,13 @@
 
   services = {
     fwupd.enable = true; # framework firmware updates through fwupd
-    udev.extraRules = ''
-      ACTION=="add", SUBSYSTEM="backlight", RUN+="${pkgs.coreutils-full}/bin/chgrp video /sys/class/backlight/%k/brightness"
-      ACTION=="add", SUBSYSTEM="backlight", RUN+="${pkgs.coreutils-full}/bin/chmod g+w video /sys/class/backlight/%k/brightness"
-      ACTION=="add", SUBSYSTEM="leds", RUN+="${pkgs.coreutils-full}/bin/chgrp video /sys/class/backlight/%k/brightness"
-      ACTION=="add", SUBSYSTEM="leds", RUN+="${pkgs.coreutils-full}/bin/chmod g+w /sys/class/backlight/%k/brightness"
-      ACTION=="change", SUBSYSTEM="backlight", OPTIONS:="nowatch"
-    ''; # allow video group to adjust backlight settings
+    #udev.extraRules = ''
+    #  ACTION=="add", SUBSYSTEM="backlight", RUN+="${pkgs.coreutils-full}/bin/chgrp video /sys/class/backlight/%k/brightness"
+    #  ACTION=="add", SUBSYSTEM="backlight", RUN+="${pkgs.coreutils-full}/bin/chmod g+w video /sys/class/backlight/%k/brightness"
+    #  ACTION=="add", SUBSYSTEM="leds", RUN+="${pkgs.coreutils-full}/bin/chgrp video /sys/class/backlight/%k/brightness"
+    #  ACTION=="add", SUBSYSTEM="leds", RUN+="${pkgs.coreutils-full}/bin/chmod g+w /sys/class/backlight/%k/brightness"
+    #  ACTION=="change", SUBSYSTEM="backlight", OPTIONS:="nowatch"
+    #''; # allow video group to adjust backlight settings
   };
 
   system.stateVersion = "23.11";
