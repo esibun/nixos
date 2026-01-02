2de47462c7dd4db0816d641a7686b512c2518204
esi <git@esibun.net>
2026-01-02 17:58:19 -0500

    oops

--- home/profiles/gaming.nix
+++ home/profiles/gaming.nix
@@ -65,7 +65,6 @@ in
         ];
         # Genshin requires cursor grab to avoid cursor state issues (camera)
         extraGamescopeFlags = "--force-grab-cursor --cursor-scale-height 1080";
-        gamePrefix = "${pkgs.mangohud}/bin/mangohud";
         # wine doesn't appreciate being given a windows executable without the correct extension
         commandPrefix = "${pkgs.coreutils}/bin/ln -sf ${inputs.genshin-fpsunlock} ~/.local/share/games/genshin/fpsunlock.exe && ";
       })
