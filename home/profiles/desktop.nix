5105ffbbcb0ebd89e90ecb403534a2e6b48f8760
esi <git@esibun.net>
2026-01-09 01:56:22 -0500

    hyprland: remove color management flag (removed in Hyprland#12693)

--- home/profiles/desktop.nix
+++ home/profiles/desktop.nix
@@ -367,9 +367,6 @@ in
         # config files don't seem to actually read
         "${pkgs.hyprland}/bin/hyprctl setcursor Nordzy-cursors-white 48"
       ];
-      experimental = {
-        xx_color_management_v4 = true;
-      };
       general = {
         gaps_out = 5;
       };
