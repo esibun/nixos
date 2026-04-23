a036b2b6ef3c94f7751b72ebf9225fc23b29c6f3
esi <git@esibun.net>
2026-04-23 19:17:01 -0400

    endfield: correct mainBinary location

--- home/profiles/gaming.nix
+++ home/profiles/gaming.nix
@@ -41,7 +41,7 @@ in
         baseDir = "${config.home.homeDirectory}/.local/share/games/endfield";
         shortname = "endfield";
         installerUrl = "https://launcher.gryphline.com/launcher/get_latest_launcher?appcode=TiaytKBUIEdoEwRT&ta=endfield&channel=6&sub_channel=6";
-        mainBinary = "1.3.0/Games.exe"; # TODO: autodetect latest version folder
+        mainBinary = "GRYPHLINK/1.3.0/Games.exe"; # TODO: autodetect latest version folder
         icon = icons.endfield;
         useUmu = true;
         customProtonPath = "${config.home.homeDirectory}/.local/share/Steam/compatibilitytools.d/dwproton-10.0-21-x86_64"; # TODO: bring dwproton into flake
