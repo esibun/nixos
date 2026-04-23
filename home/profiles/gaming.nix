a9ead1c7ede0beb349507b8408fb5f9d23f08269
esi <git@esibun.net>
2026-04-23 19:19:11 -0400

    endfield: define launcherBinary separately even though we use the same executable so we can launch outside of gamescope for now

--- home/profiles/gaming.nix
+++ home/profiles/gaming.nix
@@ -41,6 +41,7 @@ in
         baseDir = "${config.home.homeDirectory}/.local/share/games/endfield";
         shortname = "endfield";
         installerUrl = "https://launcher.gryphline.com/launcher/get_latest_launcher?appcode=TiaytKBUIEdoEwRT&ta=endfield&channel=6&sub_channel=6";
+        launcherBinary = "GRYPHLINK/1.3.0/Games.exe"; # TODO: autodetect latest version folder
         mainBinary = "GRYPHLINK/1.3.0/Games.exe"; # TODO: autodetect latest version folder
         icon = icons.endfield;
         useUmu = true;
