c13b8bb4a6b2b7ef04a1e99bf214158df56ec646
esi <git@esibun.net>
2026-09-19 17:41:32 -0400

    fix xsanity 0.97.0

--- nixos/common.nix
+++ nixos/common.nix
@@ -66,6 +66,7 @@ in
       libraries = with pkgs; [
         (pkgs.runCommand "steamrun-lib" {} "mkdir $out; ln -s ${pkgs.steam-run.fhsenv}/usr/lib64 $out/lib")
         (pkgs.runCommand "appimgrun-lib" {} "mkdir $out; ln -s ${pkgs.appimage-run.fhsenv}/usr/lib64 $out/lib")
+        glew # for xsanity
       ];
     };
   };
