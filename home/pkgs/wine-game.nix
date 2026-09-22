7f9530f9dd71cd44ee7cefbe1044061c78de564e
esi <git@esibun.net>
2026-09-21 22:30:08 -0400

    wine: fix syntax

--- home/pkgs/wine-game.nix
+++ home/pkgs/wine-game.nix
@@ -144,7 +144,7 @@ let
       ${gameExecLine}
     fi
   '';
-  script = writeShellScriptBin shortname (baseScript ''${scope} ${commandPrefix} ${pkgs.gamemode}/bin/gamemoderun ${if useGamescope then "${pkgs.gamescope}/bin/gamescope ${config.gamescopeFlags} ${extraGamescopeFlags} -- "}${gamePrefix} ${exeCommand} "${gameDir}${mainBinary}" ${gamePostfix} ${commandPostfix}'');
+  script = writeShellScriptBin shortname (baseScript ''${scope} ${commandPrefix} ${pkgs.gamemode}/bin/gamemoderun ${if useGamescope then "${pkgs.gamescope}/bin/gamescope ${config.gamescopeFlags} ${extraGamescopeFlags} -- " else ""}${gamePrefix} ${exeCommand} "${gameDir}${mainBinary}" ${gamePostfix} ${commandPostfix}'');
   launcherScript = writeShellScriptBin (shortname + "-launcher") (baseScript ''${scope} ${commandPrefix} ${pkgs.gamemode}/bin/gamemoderun ${exeCommand} "${gameDir}${launcherBinary}" ${commandPostfix}'');
 
   desktopItem = makeDesktopItem {
